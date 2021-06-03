ded1756561593ca79d1c707ba47032cc7eccbc79
DajuSar <yaoming_2000@hotmail.com>
2021-06-02 20:41:51 -0500

    AllMangas with the ecact property

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div>
       <Switch>
-        <Route path='/'>
+        <Route path='/' exact>
           <AllMangas />
         </Route>
         <Route path="/SingleManga">
